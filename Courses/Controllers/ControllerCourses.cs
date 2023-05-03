1e33395566fabd670af634fa34df5d159bf84eca
Sandru Gabriel <gabi200611@yahoo.com>
2023-05-03 20:53:58 +0300

    done

---
+++ Courses/Controllers/ControllerCourses.cs
@@ -0,0 +1,48 @@
+﻿using Courses.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Courses.Controllers
+{
+    internal class ControllerCourses
+    {
+
+        private List<Course> courses;
+
+        public ControllerCourses()
+        {
+
+            courses = new List<Course>();
+
+            load();
+
+        }
+
+        public void load()
+        {
+
+            string path = Application.StartupPath + @"/data/courses.txt";
+
+            StreamReader streamReader = new StreamReader(path);
+
+            string text;
+
+            while((text = streamReader.ReadLine()) != null)
+            {
+
+                Course a = new Course(text);
+
+                courses.Add(a);
+            }
+
+            streamReader.Close();
+        }
+
+
+    }
+}
